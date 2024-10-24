962e7d75494b4dc80f4693d10b339eb1b1f44752
ayeshakanwal-del <144205766+ayeshakanwal-del@users.noreply.github.com>
2024-10-24 19:10:36 +0500

    Adding remaining functionalities

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 
 const closeBtn = document.getElementById('closeBtn');
 const mobileMenu = document.getElementById('mobileMenu');
+const menuIcon = document.getElementById('menuIcon');
+const menuOptions = document.querySelectorAll('.menu-option');
+
+
+menuIcon.addEventListener('click', function() {
+  mobileMenu.classList.add('open');  
+});
 
 
 closeBtn.addEventListener('click', function() {
-  mobileMenu.classList.remove('open'); 
+  mobileMenu.classList.remove('open');  
 });
 
-const menuIcon = document.getElementById('menuIcon');
 
-menuIcon.addEventListener('click', function() {
-  mobileMenu.classList.add('open'); 
+menuOptions.forEach(option => {
+  option.addEventListener('click', function() {
+    mobileMenu.classList.remove('open'); 
+
+
+    const targetSection = document.querySelector(option.getAttribute('href'));
+    targetSection.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
 });
